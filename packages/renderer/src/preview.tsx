53e30fdd034dc02a18244174fc11cddb2510e0e3
chanwoo00106 <chan00106@naver.com>
2022-10-09 17:27:34 +0900

    Complete app

--- packages/renderer/src/preview.tsx
+++ packages/renderer/src/preview.tsx
@@ -3,18 +3,34 @@ import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkGfm from 'remark-gfm'
 import remarkReact from 'remark-react'
+import { defaultSchema } from 'hast-util-sanitize'
 import React from 'react'
 import 'github-markdown-css/github-markdown.css'
+import RemarkCode from './remark-code'
 
 interface Props {
   doc: string
+}
+
+const schema = {
+  ...defaultSchema,
+  attributes: {
+    ...defaultSchema.attributes,
+    code: [...(defaultSchema.attributes?.code || []), 'className']
+  }
 }
 
 const Preview: React.FC<Props> = props => {
   const md: any = unified()
     .use(remarkParse)
     .use(remarkGfm)
-    .use(remarkReact, React as any)
+    .use(remarkReact, {
+      createElement: React.createElement,
+      sanitize: schema,
+      remarkReactComponents: {
+        code: RemarkCode
+      }
+    } as any)
     .processSync(props.doc).result
 
   return <div className="preview markdown-body">{md}</div>
